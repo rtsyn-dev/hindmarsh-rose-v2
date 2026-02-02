32068a9dd7120dbd360111fc1722c760d17054b8
sergio <sergiohg818@gmail.com>
2026-02-02 21:17:51 +0100

    initial commit

---
+++ src/hindmarsh_rose_v2.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <cstddef>
+
+extern "C" {
+
+typedef struct hindmarsh_rose_v2_state hindmarsh_rose_v2_state_t;
+
+hindmarsh_rose_v2_state_t *hindmarsh_rose_v2_new();
+void hindmarsh_rose_v2_free(hindmarsh_rose_v2_state_t *state);
+void hindmarsh_rose_v2_set_config(hindmarsh_rose_v2_state_t *state,
+                                      const char *key, size_t len, double value);
+void hindmarsh_rose_v2_set_input(hindmarsh_rose_v2_state_t *state,
+                                     const char *name, size_t len,
+                                     double value);
+void hindmarsh_rose_v2_process(hindmarsh_rose_v2_state_t *state);
+double hindmarsh_rose_v2_get_output(
+    const hindmarsh_rose_v2_state_t *state, const char *name, size_t len);
+double hindmarsh_rose_v2_select_optimal_dt(double pts_match);
+double hindmarsh_rose_v2_select_pts_burst(double sec_per_burst, double freq);
+}
